c92a806ac88c792597f96cf2b00b773f5c848361
Faraaz Khan <faraaz@rationalizeit.us>
2018-08-24 09:53:04 -0500

    add hook

---
+++ circleci_validate.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+if ! [ -x "$(command -v circleci)" ]; then
+        echo 'Warning: circleci is not installed. Circle config validation will not occur.' >&2
+        exit 0
+elif [ ! -f .circleci/config.yml  ]; then
+				echo 'Warning: did not find circleci config. Circle config validation will not occur.' >&2
+				exit 0
+else
+        # The following line is needed by the CircleCI Local Build Tool (due to Docker interactivity)
+        exec < /dev/tty
+
+        # If validation fails, tell Git to stop and provide error message. Otherwise, continue.
+        if ! eMSG=$(circleci config validate -c .circleci/config.yml); then
+                echo "CircleCI Configuration Failed Validation."
+                echo $eMSG
+                exit 1
+        fi
+
+fi
